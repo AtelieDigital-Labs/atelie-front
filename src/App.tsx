5365c91eb69d968a880b2318c7bc82c05e5ee24d
MariaAless <alessandraaquino2003@gmail.com>
2026-06-07 15:40:48 -0300

    feat: Start component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 
 import { Header } from "./components/Header"
 import {FavoriteButton} from './components/ui/FavoriteButton'
+import {Stars} from './components/ui/Stars'
+
 function App() {
 
   return (
    <div>
      <Header/>
      <FavoriteButton />
-
+    <Stars rating={4} maxStars={5} reviewCount={100} />
    </div>
     
  
