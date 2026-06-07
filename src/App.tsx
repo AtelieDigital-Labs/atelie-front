735b5a8da1ee265126eedf767ff0c58acc901851
MariaAless <alessandraaquino2003@gmail.com>
2026-06-07 14:20:18 -0300

    feat: FavoriteButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import { Header } from "./components/Header"
+import {FavoriteButton} from './components/ui/FavoriteButton'
 function App() {
 
   return (
    <div>
      <Header/>
+     <FavoriteButton />
 
    </div>
     
