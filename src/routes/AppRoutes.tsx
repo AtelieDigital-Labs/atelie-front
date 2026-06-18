161d7eed750551cc492385f7b29b878f5bcfcd38
MariaAless <alessandraaquino2003@gmail.com>
2026-06-18 20:57:55 -0300

    feat: add search navigation on header and categories, and  add search results page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,16 @@ import {Routes, Route} from 'react-router-dom'
 import {AppLayout} from '../pages/_layouts/AppLayout'
 import {Home} from '../pages/app/home'
 import {ProductDetail} from '../pages/app/product-detail/index'
+import { SearchPage } from '../pages/app/search'
+
+
 export function AppRoutes(){
   return(
     <Routes>
       <Route element={<AppLayout/>}>
         <Route path='/' element={<Home/>}/>
         <Route path='product/:id' element={<ProductDetail/>}/>
+        <Route path="/search" element={<SearchPage />} />
 
       </Route>
     </Routes>
