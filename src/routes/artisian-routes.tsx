d02ef49255eefafa2bc736b7b4b833a42ebef5c9
MariaAless <alessandraaquino2003@gmail.com>
2026-07-02 15:51:30 -0300

    feat: orders and products artisan page, routes

--- src/routes/artisian-routes.tsx
+++ src/routes/artisian-routes.tsx
@@ -1,5 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import {Dashboard} from '../pages/artisan/dashboard/index'
+import {ArtisanProducts} from '../pages/artisan/products/index'
+import {ArtisanOrders} from '../pages/artisan/orders/index'
 import {AppLayout} from '../pages/_layouts/AppLayout'
 
 export function ArtisianRoutes(){
@@ -7,6 +9,8 @@ export function ArtisianRoutes(){
     <Routes>
       <Route element={<AppLayout/>}>
           <Route path='/artisan/dashboard' element={<Dashboard/>} />
+          <Route path='/artisan/products' element={<ArtisanProducts/>} />
+          <Route path='/artisan/orders' element={<ArtisanOrders/>} />
       </Route>
     </Routes>
   )
